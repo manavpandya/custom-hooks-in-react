cba1a1436b6ab57914324956336af3e4c7bfd543
manavpandya <pandya.manav@gmail.com>
2019-05-27 22:03:07 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -1,11 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { render } from 'react-dom';
+import CustomHook  from './CustomHook';
 
 function App() {
+  const users = CustomHook("https://jsonplaceholder.typicode.com/users");
+  console.log(users)
 
   return (
     <div>
-      
+      <div class="container">
+        <div class="jumbotron">
+          <h1>Custom Hooks Example</h1>
+        </div>
+        <div>
+          <table class="table table-hover">
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>User name</th>
+                <th>Email</th>
+                <th>Website</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((item, index) => {
+                return (<tr key={index}>
+                  <td>{item.name}</td>
+                  <td>{item.username}</td>
+                  <td>{item.email}</td>
+                  <td>{item.website}</td>
+                </tr>)
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </div>
   );
 }
